650c642b23bc29cb47b89b144f2652b8c9ca672f
Andras Karsai <andykarsai@gmail.com>
2026-04-05 21:01:50 -0400

    Got to implementing unit propagate

--- src/sat.py
+++ src/sat.py
@@ -8,7 +8,19 @@ from sat_struct import (
 
 
 def unit_propagate(unit_clause: tuple, clauses: list[tuple]) -> list[tuple]:
-    """_summary_
+    """This function takes a unit clause and the current list of clauses, and
+    returns an updated list of clauses after performing unit propagation based
+    on the unit clause.
+    If a clause is a unit clause, i.e. it contains only
+     a single unassigned literal, this clause can only be
+     satisfied by assigning the necessary value to make this
+    literal true. Thus, no choice is necessary. Unit
+    propagation consists in removing every clause containing
+    a unit clause's literal and in discarding the
+    complement of a unit clause's literal from every
+    clause containing that complement. In practice,
+    this often leads to deterministic cascades of units,
+    thus avoiding a large part of the naive search space.
 
     Parameters
     ----------
@@ -28,6 +40,62 @@ def unit_propagate(unit_clause: tuple, clauses: list[tuple]) -> list[tuple]:
 
 
 def get_unit_clauses(clauses: list[tuple], assignments: bytearray) -> list[tuple]:
+    """Returns a list of unit clauses from the input clause list based on the current variable assignments.
+    A unit clause is a clause that has exactly one unassigned literal and all other literals are falsified by the current assignment.
+
+    Parameters
+    ----------
+    clauses : list[tuple]
+        List of tuples representing the clauses, where each tuple contains integers representing literals (positive for true, negative for false).
+    assignments : bytearray
+        Bytearray representing the current variable assignments. Each index corresponds to a variable, and the value can be UNASSIGNED, TRUE, or FALSE.
+
+    Returns
+    -------
+    list[tuple]
+        List of unit clauses.
+    """
+    unit_clauses: list[tuple] = []
+    true_mask, false_mask = get_assignment_bitmasks(assignments)
+    for clause in clauses:
+        clause_p_mask, clause_n_mask = get_clause_bitmasks(clause)
+        if is_unit(clause_p_mask, clause_n_mask, true_mask, false_mask):
+            unit_clauses.append(clause)
+    return unit_clauses
+
+
+def is_unit(
+    clause_p_mask: int, clause_n_mask: int, true_mask: int, false_mask: int
+) -> bool:
+    """Checks if an input clause is a unit clause.
+
+    Parameters
+    ----------
+    clause_p_mask : int
+        Bitmask representing the positive literals in the clause. A bit is 1 if the variable appears as a positive literal, otherwise it's zero.
+    clause_n_mask : int
+        Bitmask representing the negative literals in the clause. A bit is 1 if the variable appears as a negative literal, otherwise it's zero.
+    true_mask : int
+        Bitmask representing the variables assigned TRUE in the current assignment.
+    false_mask : int
+        Bitmask representing the variables assigned FALSE in the current assignment.
+
+    Returns
+    -------
+    bool
+        Returns True if the clause is a unit clause, False otherwise.
+    """
+    # 1. If a clause is already satisfied, then it is not unit.
+    if true_mask & clause_p_mask or false_mask & clause_n_mask:
+        return False
+    # 2. Get literals that are not yet falsified
+    remaining_lits = (clause_p_mask & ~false_mask) | (clause_n_mask & ~true_mask)
+    # 3. Check if exactly one bit is set == exactly one literal is not yet falsified
+    # (n > 0 and n & (n - 1) == 0 is the classic bit-trick for power of 2)
+    return remaining_lits > 0 and (remaining_lits & (remaining_lits - 1)) == 0
+
+
+def dpll(clauses: list[tuple], assignments: bytearray) -> list[tuple]:
     """_summary_
 
     Parameters
@@ -42,55 +110,36 @@ def get_unit_clauses(clauses: list[tuple], assignments: bytearray) -> list[tuple
     list[tuple]
         _description_
     """
-    true_mask, false_mask = get_assignment_bitmasks(assignments)
-    for clause in clauses:
-        clause_p_mask, clause_n_mask = get_clause_bitmasks(clause)
-        if is_unit(clause_p_mask, clause_n_mask, true_mask, false_mask):
-            print(f"Found unit clause: {clause}")
-    unit_clauses = []
-    return unit_clauses
+    unit_clauses = get_unit_clauses(clauses, assignments)
+    #  Unit clause propagation
+    for unit_clause in unit_clauses:
+        clauses = unit_propagate(unit_clause, clauses)
 
+    # Pure literal elimination would go here (not implemented yet)
+    # while there is a literal l that occurs pure in Φ do
+    # Φ ← pure-literal-assign(l, Φ);
 
-def is_unit(
-    clause_p_mask: int, clause_n_mask: int, true_mask: int, false_mask: int
-) -> bool:
-    """Checks if an input clause is a unit clause.
-
-    Parameters
-    ----------
-    clause_p_mask : int
-        _description_
-    clause_n_mask : int
-        _description_
-    true_mask : int
-        _description_
-    false_mask : int
-        _description_
-
-    Returns
-    -------
-    bool
-        _description_
-    """
-    # 1. If a clause is already satisfied, then it is not unit.
-    if true_mask & clause_p_mask or false_mask & clause_n_mask:
-        return False
-    # 2. Get literals that are not yet falsified
-    remaining_lits = (clause_p_mask & ~false_mask) | (clause_n_mask & ~true_mask)
-    # 3. Check if exactly one bit is set
-    # (n > 0 and n & (n - 1) == 0 is the classic bit-trick for power of 2)
-    
-    return False
+    # Stopping conditions for DPLL go here (e.g. if clauses is empty, or if any clause is empty, etc.)
+    if not clauses:
+        # If there are no clauses left, then the formula is satisfied by the current assignment
+        print("SAT")
+        # TODO: Print the satisfying assignment
+    if clauses and any(len(clause) == 0 for clause in clauses):
+        # If there are clauses left but any clause is empty, then the formula is unsatisfiable under the current assignment
+        print("UNSAT")
+    # DPLL Procedure code goes here (e.g. choose a variable, assign it, recurse, backtrack, etc.)
+    # TODO: Implement a better heuristic for choosing a literal
+    # return dpll(clauses, assignments) or dpll(clauses, assignments)
+    raise NotImplementedError("DPLL procedure not implemented yet.")
 
 
 if __name__ == "__main__":
     # Load in target file
-    clauses, num_vars, num_cls = parse_dimacs_cnf(Path("examples/simple_v3_v2.cnf"))
+    clauses, num_vars, num_cls = parse_dimacs_cnf(
+        Path("benchmarks\\uf20-91\\uf20-010.cnf")
+    )
     assignments = create_assignment_array(num_vars)
-    # true_assign_mask, false_assign_mask = get_assignment_bitmasks(assignments)
-    # Retreive the list of unit clauses in the current clause list
-    unit_clauses = get_unit_clauses(clauses, assignments)
-    #  Unit propagation
-    for unit_clause in unit_clauses:
-        clauses = unit_propagate(unit_clause, clauses)
-    print("Hello")
+    # Retrieve the list of unit clauses in the current clause list
+    dpll(clauses, assignments)
+
+    print("Done")
--- src/sat_struct.py
+++ src/sat_struct.py
@@ -5,23 +5,30 @@ UNASSIGNED = 0
 TRUE = 1
 FALSE = 2
 
+
 def create_assignment_array(num_vars: int) -> bytearray:
-    """_summary_
+    """Creates an array to hold the variable assignments, initialized to UNASSIGNED.
+    This array will be indexed by variable number (0-based index for variable 1, etc.)
+    and will hold the assignment state (UNASSIGNED, TRUE, FALSE) for each variable.
+    This function is useful for initializing the assignment state before starting the DPLL
+    algorithm. It should really be only used once at the beginning of the algorithm, and
+    then the same array should be updated in place as the algorithm progresses.
 
     Parameters
     ----------
     num_vars : int
-        _description_
+        Number of variables in the SAT problem, used to determine the size of the assignment array.
 
     Returns
     -------
     bytearray
-        _description_
+        Array initialized with UNASSIGNED values.
     """
     return bytearray([UNASSIGNED] * num_vars)
 
-def get_assignment_bitmasks(assignment: bytearray) -> tuple[int,int]:
-    """ Returns a tuple of int 
+
+def get_assignment_bitmasks(assignment: bytearray) -> tuple[int, int]:
+    """Returns a tuple of int
     This operation can be expensive so only call when really necessary.
 
     Parameters
@@ -34,27 +41,32 @@ def get_assignment_bitmasks(assignment: bytearray) -> tuple[int,int]:
     tuple[int,int]
         _description_
     """
-    
-    true_mask_list: list[int] = [lit & TRUE for lit in assignment]  # Bits of variables assigned TRUE
-    false_mask_list: list[int] = [lit & FALSE for lit in assignment]  # Bits of variables assigned FALSE
+
+    true_mask_list: list[int] = [
+        lit & TRUE for lit in assignment
+    ]  # Bits of variables assigned TRUE
+    false_mask_list: list[int] = [
+        lit & FALSE for lit in assignment
+    ]  # Bits of variables assigned FALSE
     true_mask: int = int("".join(map(str, true_mask_list)))
     false_mask: int = int("".join(map(str, false_mask_list)))
     return (true_mask, false_mask)
 
-def get_clause_bitmasks(clause: tuple[int]) -> tuple[int,int]:
-    """ In these bitmasks, for the positive mask a bit is 1 if the variable appears
+
+def get_clause_bitmasks(clause: tuple[int]) -> tuple[int, int]:
+    """In these bitmasks, for the positive mask a bit is 1 if the variable appears
     as a positive literal, otherwise it's zero. For the negative mask a bit is 1 if the
     variable appears as a negative literal, otherwise it's zero.
 
     Parameters
     ----------
     clause : tuple[int]
-        _description_
+        Tuple of integers representing the literals in the clause. Positive integers represent positive literals, and negative integers represent negative literals.
 
     Returns
     -------
     tuple[int,int]
-        _description_
+        Tuple of bitmasks (p_mask, n_mask) where p_mask is the bitmask for positive literals and n_mask is the bitmask for negative literals.
     """
     p_mask: int = 0
     n_mask: int = 0
@@ -71,10 +83,10 @@ def get_clause_bitmasks(clause: tuple[int]) -> tuple[int,int]:
 #     var = lit >> 1
 #     is_negated = lit & 1
 #     val = assigns[var]
-    
+
 #     if val == UNASSIGNED:
 #         return False
-    
+
 #     # If parity is 0 (pos), we need TRUE (1)
 #     # If parity is 1 (neg), we need FALSE (2)
 #     target = 2 if is_negated else 1
