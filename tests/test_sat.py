78d2d55bc80c25a1df4c0d927d4c12a5cf54cee0
Andy K. <Dmirtao@users.noreply.github.com>
2026-05-07 20:02:00 -0400

    Update tests for more flexibility

--- tests/test_sat.py
+++ tests/test_sat.py
@@ -3,9 +3,11 @@ from sat_struct import init_assignment_array
 from sat import cdcl
 import os
 from pathlib import Path
+import cProfile
 
-
-def run_benchmark_folder(benchmark_path: Path, subset: int = 0) -> tuple[int, int]:
+def run_benchmark_folder(
+    benchmark_path: Path, subset: int = 0, use_cdl=True, use_wl=False
+) -> tuple[int, int]:
     was_sat = []
     was_unsat = []
     # Load in target file
@@ -16,7 +18,9 @@ def run_benchmark_folder(benchmark_path: Path, subset: int = 0) -> tuple[int, in
                 return (len(was_sat), len(was_unsat))
             clauses, num_vars, num_cls = parse_dimacs_cnf(os.path.join(root, name))
             assignments = init_assignment_array(num_vars)
-            sat, final_assignments = cdcl(clauses, assignments)
+            sat, final_assignments = cdcl(
+                clauses, assignments, num_vars=num_vars, use_cdl=use_cdl, use_wl=use_wl
+            )
             if sat:
                 was_sat.append(name)
             else:
@@ -25,20 +29,26 @@ def run_benchmark_folder(benchmark_path: Path, subset: int = 0) -> tuple[int, in
 
 
 def test_uf20_91():
-    (was_sat, was_unsat) = run_benchmark_folder(Path("benchmarks/uf20-91/"))
+    (was_sat, was_unsat) = run_benchmark_folder(
+        Path("benchmarks/uf20-91/"), use_wl=True
+    )
     assert was_sat == 1000
     assert was_unsat == 0
 
 
 def test_uf100_430():
-    (was_sat, was_unsat) = run_benchmark_folder(Path("benchmarks/uf100-430/"), subset=5)
-    assert was_sat == 5
+    subset_count = 10
+    (was_sat, was_unsat) = run_benchmark_folder(
+        Path("benchmarks/uf100-430/"), subset=subset_count, use_wl=True
+    )
+    assert was_sat == subset_count
     assert was_unsat == 0
 
 
 def test_uuf100_430():
+    subset_count = 10
     (was_sat, was_unsat) = run_benchmark_folder(
-        Path("benchmarks/uuf100-430/"), subset=5
+        Path("benchmarks/uuf100-430/"), subset=subset_count, use_wl=True
     )
     assert was_sat == 0
-    assert was_unsat == 5
+    assert was_unsat == subset_count
