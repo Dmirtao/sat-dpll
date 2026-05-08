8f159a9cb036ae76d5dfe54441a2899f2508e348
Andy K. <Dmirtao@users.noreply.github.com>
2026-05-07 23:00:42 -0400

    Fix subsetting and add progress bar

--- tests/test_sat.py
+++ tests/test_sat.py
@@ -6,6 +6,7 @@ from pathlib import Path
 import cProfile
 import pstats
 import pytest
+from tqdm import tqdm
 
 @pytest.fixture(autouse=True)
 def profile_test(request):
@@ -37,10 +38,9 @@ def run_benchmark_folder(
     was_unsat = []
     # Load in target file
     for root, dirs, files in os.walk(benchmark_path):
-        for idx, name in enumerate(files):
-            # Exit condition if a subset was defined.
-            if subset > 0 and idx >= subset:
-                return (len(was_sat), len(was_unsat))
+        if subset > 0:
+            files = files[:subset]
+        for idx, name in enumerate(tqdm(files, desc=f"Benchmarking {benchmark_path.name}")):
             clauses, num_vars, num_cls = parse_dimacs_cnf(os.path.join(root, name))
             assignments = init_assignment_array(num_vars)
             sat, final_assignments = cdcl(
@@ -53,27 +53,27 @@ def run_benchmark_folder(
     return (len(was_sat), len(was_unsat))
 
 
-def test_uf20_91(capsys):
+def test_uf20_91():
     (was_sat, was_unsat) = run_benchmark_folder(
-        Path("benchmarks/uf20-91/"), use_wl=True
+        Path("benchmarks/uf20-91/"), use_wl=False
     )
     assert was_sat == 1000
     assert was_unsat == 0
 
 
 def test_uf100_430():
-    subset_count = 100
+    subset_count = 10
     (was_sat, was_unsat) = run_benchmark_folder(
-        Path("benchmarks/uf100-430/"), subset=subset_count, use_wl=True
+        Path("benchmarks/uf100-430/"), subset=subset_count, use_wl=False
     )
     assert was_sat == subset_count
     assert was_unsat == 0
 
 
 def test_uuf100_430():
-    subset_count = 100
+    subset_count = 10
     (was_sat, was_unsat) = run_benchmark_folder(
-        Path("benchmarks/uuf100-430/"), subset=subset_count, use_wl=True
+        Path("benchmarks/uuf100-430/"), subset=subset_count, use_wl=False
     )
     assert was_sat == 0
     assert was_unsat == subset_count
